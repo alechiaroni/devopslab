05074f399acde09806b165b6168d8ba07c59ee3b
alechiaroni <105027013+alechiaroni@users.noreply.github.com>
2023-04-20 13:50:40 -0300

    Phase 5 delivery

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps"
+    return "Entrega fase 5"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
