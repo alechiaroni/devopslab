4568a87459117d6314e278f2c22836a1a50dbd34
alechiaroni <105027013+alechiaroni@users.noreply.github.com>
2023-04-20 14:35:55 -0300

    Entrega Fase 5

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Entrega fase 5"
+    return "Entrega Fase 5"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
